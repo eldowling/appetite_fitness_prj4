48ade8cb48f1b2b5c8fa412dbbb4e3953465d61c
eldowling <elinordowling@gmail.com>
2020-07-16 20:45:36 +0000

    Updated products page with JS for sorting, and back to top button. Formatted button with CSS. Removed Categories filter in views.py as not needed, using subcategories instead.

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category, Subcategory, Sizes, Subscription_Type, Product_Subscription
 
 # Create your views here.
@@ -9,7 +10,6 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    categories = None
     subcategories = None
     sort = None
     direction = None
@@ -28,11 +28,6 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-        
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            products = products.filter(category__code__in=categories)
-            categories = Category.objects.filter(code__in=categories)
         
         if 'subcategory' in request.GET:
             subcategories = request.GET['subcategory'].split(',')
@@ -54,7 +49,6 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
-        'current_categories': categories,
         'current_subcategories': subcategories,
         'current_sorting': current_sorting,
     }
