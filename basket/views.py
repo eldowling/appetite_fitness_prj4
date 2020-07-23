85d50166a58761d5b30a6c4cb86b472003fe8e8e
eldowling <elinordowling@gmail.com>
2020-07-23 21:46:49 +0000

    Update to products/views.py to remove the refresh_subscription view and use product_detail view for the update link on a selected subscription. Updates to subscription_form.html to match this

--- basket/views.py
+++ basket/views.py
@@ -17,13 +17,21 @@ def add_to_basket(request, item_id):
 
     product = get_object_or_404(Product, pk=item_id)
     subscription = None
-    if 'purchase_subscription_id' in request.POST:
-        subscription = request.POST.get('purchase_subscription_id')
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    print("starting add_to_basket")
+    if 'purchase_subscription_id' in request.POST:
+        print("purchase_subscription_id is in the request.Post")
+        subscription = request.POST.get('purchase_subscription_id')    
     basket = request.session.get('basket', {})
+    print("subscription, quantity, redirect_url")
+    print(subscription)
+    print(quantity)
+    print(redirect_url)
 
     if subscription:
+        print("subscription is true, item_id")
+        print(item_id)
         if item_id in list(basket.keys()):
             if subscription in basket[item_id]['items_subscription'].keys():
                 basket[item_id]['items_subscription'][subscription] += quantity
--- products/views.py
+++ products/views.py
@@ -57,30 +57,16 @@ def all_products(request):
 
 def product_detail(request, product_id):
     """ A view to show a single products details """
-
     product = get_object_or_404(Product, pk=product_id)
     product_subscription = product.product_subscription_set.all()
     # Default to the first subscription and display the price for this subscription
     selected_subscription = Subscription_Type.objects.filter(id=product_id).first()
     sel_product_subscription = product_subscription.filter(subscription_type=selected_subscription)
 
-    context = {
-        'product': product,
-        'product_subscription': product_subscription,
-        'selected_subscription': selected_subscription,
-        'sel_product_subscription': sel_product_subscription,
-    }
-
-    return render(request, 'products/product_detail.html', context)
-
-def refresh_subscription(request, product_id):
-    """ A view to refresh the selected subscription type in products details """
-
-    product = get_object_or_404(Product, pk=product_id)
-    product_subscription = product.product_subscription_set.all()
-    selected_subscription = request.POST.get("purchase_subscription_id")
-    sel_product_subscription = product_subscription.filter(subscription_type=selected_subscription)
-    
+    if request.POST:
+        selected_subscription = request.POST.get("purchase_subscription_id")
+        sel_product_subscription = product_subscription.filter(subscription_type=selected_subscription)
+        
     context = {
         'product': product,
         'product_subscription': product_subscription,
