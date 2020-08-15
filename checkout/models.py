2edcad0caa7423b5123dd7ac9a57ccb7c6a04603
eldowling <elinordowling@gmail.com>
2020-08-15 00:49:59 +0000

    Added order details to checkout_success.html

--- checkout/models.py
+++ checkout/models.py
@@ -48,8 +48,10 @@ class Order(models.Model):
             self.delivery_cost = self.order_total * sdp / 100
         else:
             self.delivery_cost = 0
-        #self.non_delivery_total = self.lineitems.aggregate(delivery_charge=True(
-        #    Sum('lineitem_total')))['lineitem_total__sum'] or 0
+            
+        #if self.deliver_charge:
+            #self.non_delivery_total = self.lineitems.aggregate(delivery_charge=True(
+            #    Sum('lineitem_total')))['lineitem_total__sum'] or 0
         self.grand_total = self.order_total + self.non_delivery_total + self.delivery_cost
         self.save()
 
